968f65250a1794e5ef39b4c35ae1bd471a191935
LuK1337 <priv.luk@gmail.com>
2022-04-15 10:54:44 +0200

    oplus: Add fp_state sysfs
    This lets us to poll it in userspace.
    
    Change-Id: I999d165b3b872475d793a60f5fac5e5d97355e11

--- qcom/opensource/display-drivers/oplus/oplus_onscreenfingerprint.c
+++ qcom/opensource/display-drivers/oplus/oplus_onscreenfingerprint.c
@@ -15,7 +15,7 @@
 #include "sde_encoder_phys.h"
 #include "sde_trace.h"
 #include <linux/msm_drm_notify.h>
-#include "../../../../../sm8550/drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_notify/touchpanel_event_notify.h"
+#include "touchpanel_event_notify.h"
 
 #ifdef OPLUS_FEATURE_DISPLAY_ADFR
 #include "oplus_adfr.h"
